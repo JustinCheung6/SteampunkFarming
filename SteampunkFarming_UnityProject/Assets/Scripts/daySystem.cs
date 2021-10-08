7918bb167ec6f1b913415223c2684f0850826c84
Joshua Correa <correajoshua2@gmail.com>
2021-10-08 15:57:49 -0700

    JoshuaTestScene & daySystem script (not finished).

--- SteampunkFarming_UnityProject/Assets/Scripts/daySystem.cs
+++ SteampunkFarming_UnityProject/Assets/Scripts/daySystem.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class daySystem : MonoBehaviour
 {
+    enum Time {Morning, Noon, Night}
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
